9ab95ba5f4e1ed005f0a28ecab70bbb7f58c8523
Ngai Elizabeth <asobingai@gmail.com>
2024-01-13 14:10:28 +0100

    Service discovery

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Build project
+
+
+# Containerize
+FROM openjdk:11
+
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
+EXPOSE 18761
---
+++ src/test/java/com/example/serviceregistry/serviceresgistry/ServiceResgistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistry.serviceresgistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceResgistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
